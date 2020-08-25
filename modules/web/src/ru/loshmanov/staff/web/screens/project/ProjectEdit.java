65785012aa99cb8773efb2d7d4f64c5850b5642a
pppkms2339 <pppkms2339@gmail.com>
2020-08-25 17:50:54 +1000

    Реализация без списка List<Employee> отмеченных работников

--- modules/web/src/ru/loshmanov/staff/web/screens/project/ProjectEdit.java
+++ modules/web/src/ru/loshmanov/staff/web/screens/project/ProjectEdit.java
@@ -1,8 +1,8 @@
 package ru.loshmanov.staff.web.screens.project;
 
-import com.haulmont.cuba.gui.Screens;
 import com.haulmont.cuba.gui.UiComponents;
-import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.CheckBox;
+import com.haulmont.cuba.gui.components.Component;
 import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
 import ru.loshmanov.staff.entity.Employee;
@@ -11,8 +11,6 @@ import ru.loshmanov.staff.entity.Project;
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.apache.poi.sl.usermodel.PresetColor.Window;
 
 @UiController("staff_Project.edit")
 @UiDescriptor("project-edit.xml")
@@ -23,45 +21,20 @@ public class ProjectEdit extends StandardEditor<Project> {
     @Inject
     private UiComponents uiComponents;
 
-    @Inject
-    private Screens screens;
-
-    private List<Employee> selectedItemsList = new ArrayList<>();
-
     public Component generateCheckBox(Employee item) {
         CheckBox checkBox = uiComponents.create(CheckBox.NAME);
-        if (selectedItemsList.contains(item)) {
+        List<Employee> employees = getEditedEntityContainer().getItem().getEmployees();
+        if (employees.contains(item)) {
             checkBox.setValue(true);
         }
         checkBox.addValueChangeListener(e -> {
-            //List<Project> list = item.getProjects();
             if (e.getValue().equals(Boolean.TRUE)) {
-                //list.add;
-                selectedItemsList.add(item);
+                employees.add(item);
             } else {
-                selectedItemsList.remove(item);
+                employees.remove(item);
             }
         });
         return checkBox;
     }
 
-    @Subscribe(id = "employeesDc", target = Target.DATA_CONTAINER)
-    public void onEmployeesDcCollectionChange(CollectionContainer.CollectionChangeEvent<Employee> event) {
-        if (selectedItemsList.size() != 0) {
-            return;
-        }
-        selectedItemsList.addAll(event.getSource().getItems());
-    }
-
-//    @Subscribe("okButton")
-//    public void onOkButtonClick(Button.ClickEvent event) {
-//        Project project = getEditedEntityContainer().getItem();
-//        project.setEmployees(selectedItemsList);
-//    }
-
-    public void save() {
-        Project project = getEditedEntityContainer().getItem();
-        project.setEmployees(selectedItemsList);
-        closeWithCommit();
-    }
 }
